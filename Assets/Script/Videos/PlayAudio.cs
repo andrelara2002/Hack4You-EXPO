86513b2a65354ce94c3ec6fc67b1c49ee40745be
André Lara <66387919+andrelara2002@users.noreply.github.com>
2021-04-26 11:33:51 -0300

    Introduzindo controles sonoros e triggers

---
+++ Assets/Script/Videos/PlayAudio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    public AudioSource sound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (sound.isPlaying)
+        {
+            sound.Pause();
+        }
+        else
+        {
+            sound.Play();
+        }
+    }
+}
---
+++ Assets/Script/Videos/PlayVideo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideo : MonoBehaviour
+{
+
+    public GameObject videoPlayer;
+    public int timeToStop;
+
+    // Use this for initialization
+    void Start()
+    {
+        videoPlayer.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        videoPlayer.SetActive(true);
+        Destroy(videoPlayer, timeToStop);
+    }
+}
