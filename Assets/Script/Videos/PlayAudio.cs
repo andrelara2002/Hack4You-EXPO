f77813ac4b3c740df41ea8bfa6001dd929a7e8c3
André Lara <66387919+andrelara2002@users.noreply.github.com>
2021-04-28 12:16:11 -0300

    Adicionando sala principal

--- Assets/Script/Videos/PlayAudio.cs
+++ Assets/Script/Videos/PlayAudio.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class PlayAudio : MonoBehaviour
 {
     public AudioSource sound;
+    public AudioSource music;
+
+    GameObject soundObject = GameObject.Find("BackgroundMusic");
 
     private void OnTriggerEnter(Collider other)
     {
 
+        AudioSource backgroundMusic = soundObject.GetComponent<AudioSource>();
+
         if (sound.isPlaying)
         {
             sound.Pause();
+            music.Pause();
         }
         else
         {
+            music.Play();
             sound.Play();
         }
     }
